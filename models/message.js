80d4a65041efb0aedeeefa7f75d657dee1e1485d
unknown <kareldrnec@gmail.com>
2021-11-11 00:53:54 +0100

    pridan model message

---
+++ models/message.js
@@ -0,0 +1,13 @@
+//schema of message
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var messageSchema = new Schema({
+
+
+
+
+});
+
+module.exports = mongoose.model("Message", messageSchema);
