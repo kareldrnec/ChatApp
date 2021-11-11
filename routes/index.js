3f13e9efc9752049d8bef1230fd7a60f0c99b8db
unknown <kareldrnec@gmail.com>
2021-11-11 11:56:35 +0100

    pridany partials

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var auth = require('../auth');
 
-router.get('/', auth.requiresLogin, function(req, res){
+router.get('/', function(req, res){
     res.render('index');
 });
 
