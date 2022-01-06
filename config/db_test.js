9b685d9d45dc4a1c81d90f9e87b3f4666d55a1f3
unknown <kareldrnec@gmail.com>
2022-01-06 13:07:24 +0100

    update .env

--- config/db_test.js
+++ config/db_test.js
@@ -1,16 +1,15 @@
-
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://user1234:user1234@cluster0.gr9ky.mongodb.net/PWA_test?retryWrites=true&w=majority";
+const mongoURI = process.env.MONGODB_URI_TEST;
 
-module.exports.connect = async () => {
+module.exports.connect = async() => {
     const mongooseOpts = {
         useNewUrlParser: true
     };
     await mongoose.connect(mongoURI, mongooseOpts);
 }
 
-module.exports.clearDatabase = async () => {
+module.exports.clearDatabase = async() => {
     const collections = mongoose.connection.collections;
     for (const key in collections) {
         const collection = collections[key];
@@ -18,7 +17,7 @@ module.exports.clearDatabase = async () => {
     }
 }
 
-module.exports.closeDatabase = async () => {
+module.exports.closeDatabase = async() => {
     await mongoose.connection.dropDatabase();
     await mongoose.connection.close();
 }
